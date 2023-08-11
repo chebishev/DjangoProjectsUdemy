852138a96b49027163d5df6aa8269244842cbb6d
chebishev <atanas.chebishev@gmail.com>
2023-08-11 15:26:38 +0300

    Added pdfkit and wkthmltopdf The installation is different for each OS check: https://github.com/JazzCore/python-pdfkit/wiki/Installing-wkhtmltopdf

--- WebBasedCVGenerator/cv_generator/pdf/admin.py
+++ WebBasedCVGenerator/cv_generator/pdf/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'phone', 'summary']
+    filter_fields = ['name', 'email', 'phone', 'summary']
+    ordering = ['name']
+
+
+admin.site.register(Profile, ProfileAdmin)
--- WebBasedCVGenerator/cv_generator/pdf/views.py
+++ WebBasedCVGenerator/cv_generator/pdf/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from .models import Profile
+from django.http import HttpResponse
+from django.template import loader
+import io
+import pdfkit
+
 
 # Create your views here.
+def accept(request):
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        summary = request.POST.get("summary")
+        degree = request.POST.get("degree")
+        school = request.POST.get("school")
+        university = request.POST.get("university")
+        previous_work = request.POST.get("previous_work")
+        skills = request.POST.get("skills")
+
+        profile = Profile(
+            name=name,
+            email=email,
+            phone=phone,
+            summary=summary,
+            degree=degree,
+            school=school,
+            university=university,
+            previous_work=previous_work,
+            skills=skills
+        )
+        profile.save()
+
+    return render(request, 'pdf/accept.html')
+
+
+def resume(request, id):
+
+    user_profile = Profile.objects.get(pk=id)
+    template = loader.get_template('pdf/resume.html')
+    html = template.render({'user_profile': user_profile})
+    options = {
+        'page-size': 'Letter',
+        'encoding': 'UTF-8',
+    }
+
+    path_wkthmltopdf = b'C:\Program Files\wkhtmltopdf\\bin\wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
+    pdf = pdfkit.from_string(html, False, options, configuration=config)
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="resume.pdf"'
+
+    return response
