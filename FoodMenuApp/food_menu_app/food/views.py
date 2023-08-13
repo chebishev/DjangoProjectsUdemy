e03d1308ae3dc68c988ec9ac8b5b9e5cd570edc2
chebishev <atanas.chebishev@gmail.com>
2023-08-13 19:16:25 +0300

    Added models and admin

--- FoodMenuApp/food_menu_app/food/views.py
+++ FoodMenuApp/food_menu_app/food/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the food index.")
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
 
 
 def item(request):
