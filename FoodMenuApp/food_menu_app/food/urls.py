c16c3d7fddf46ef28959f3dbbdd69ad1b64f69f3
chebishev <atanas.chebishev@gmail.com>
2023-08-15 15:15:43 +0300

    Added CRUD for food items and basic design

--- FoodMenuApp/food_menu_app/food/urls.py
+++ FoodMenuApp/food_menu_app/food/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
+    path('add/', views.create_item, name='create_item'),
+    path('edit/<int:id>/', views.edit_item, name='edit_item'),
+    path('delete/<int:id>/', views.delete_item, name='delete_item'),
 ]
