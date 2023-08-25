7f9e104bc64e63c8ee7e1a017052ef322623eca2
chebishev <atanas.chebishev@gmail.com>
2023-08-25 22:09:32 +0300

    Added profile model, url for image, media root

--- FoodMenuApp/food_menu_app/food_menu_app/settings.py
+++ FoodMenuApp/food_menu_app/food_menu_app/settings.py
@@ -130,3 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'index'
 # LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
+
+MEDIA_ROOT = BASE_DIR / 'pictures'
+MEDIA_URL = '/pictures/'
