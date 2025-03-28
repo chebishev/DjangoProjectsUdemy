34763865d50b858a9b1fe9c8e893a1742539315f
chebishev <atanas.chebishev@gmail.com>
2025-03-28 14:33:14 +0200

    add edit functionallity: template, view, url, styling

--- AdvancedExpenseTracker/expense_tracker/main_app/views.py
+++ AdvancedExpenseTracker/expense_tracker/main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ExpenseForm
 from .models import Expense
 
@@ -16,3 +16,15 @@ def index(request):
             'expenses': expenses
             }
         return render(request, 'main_app/index.html', context)
+
+def edit(request, id):
+    current_expense = Expense.objects.get(id=id)
+    form = ExpenseForm(instance=current_expense)
+    if request.method == 'POST':
+        expense = Expense.objects.get(id=id)
+        form = ExpenseForm(request.POST, instance=current_expense)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    return render(request, 'main_app/edit.html', {'form': form})
