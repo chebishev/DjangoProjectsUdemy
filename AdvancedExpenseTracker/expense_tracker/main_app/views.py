dd5bd346ba18573e65afee0c94b1fbe0f928f90a
chebishev <atanas.chebishev@gmail.com>
2025-03-28 12:26:06 +0200

    create expense form

--- AdvancedExpenseTracker/expense_tracker/main_app/views.py
+++ AdvancedExpenseTracker/expense_tracker/main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .forms import ExpenseForm
 # Create your views here.
 def index(request):
-    return render(request, 'main_app/index.html')
+    form = ExpenseForm()
+    return render(request, 'main_app/index.html', {'form': form})
