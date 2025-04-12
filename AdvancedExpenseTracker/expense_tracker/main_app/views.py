52bb50634491f22e7374e5afea90f4d01282a7c6
chebishev <atanas.chebishev@gmail.com>
2025-04-12 12:58:48 +0300

    add Delete functionallity

--- AdvancedExpenseTracker/expense_tracker/main_app/views.py
+++ AdvancedExpenseTracker/expense_tracker/main_app/views.py
@@ -4,27 +4,35 @@ from .models import Expense
 
 
 def index(request):
-        expenses = Expense.objects.all()
-        if request.method == 'POST':
-            form = ExpenseForm(request.POST)
-            if form.is_valid():
-                form.save()
-        
-        form = ExpenseForm()
-        context = {
-            'form': form, 
-            'expenses': expenses
-            }
-        return render(request, 'main_app/index.html', context)
+    if request.method == 'POST':
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+    
+    expenses = Expense.objects.all()
+    expense_form = ExpenseForm()
+    context = {
+        'form': expense_form, 
+        'expenses': expenses
+        }
+    return render(request, 'main_app/index.html', context)
 
 def edit(request, id):
     current_expense = Expense.objects.get(id=id)
     form = ExpenseForm(instance=current_expense)
     if request.method == 'POST':
         expense = Expense.objects.get(id=id)
-        form = ExpenseForm(request.POST, instance=current_expense)
+        form = ExpenseForm(request.POST, instance=expense)
         if form.is_valid():
             form.save()
             return redirect('index')
 
     return render(request, 'main_app/edit.html', {'form': form})
+
+
+def delete(request, id):
+    if request.method == 'POST' and 'delete' in request.POST:
+        expense = Expense.objects.get(id=id)
+        expense.delete()
+    
+    return redirect('index')
