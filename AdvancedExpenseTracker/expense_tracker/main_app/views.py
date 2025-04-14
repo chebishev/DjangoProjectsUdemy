2304e06c67eb0038f5e84afec1c72e50e35e028c
chebishev <atanas.chebishev@gmail.com>
2025-04-14 12:50:37 +0300

    add yearly, monthly, weekly sum functionalllity

--- AdvancedExpenseTracker/expense_tracker/main_app/views.py
+++ AdvancedExpenseTracker/expense_tracker/main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import ExpenseForm
 from .models import Expense
+import datetime
 from django.db.models import Sum
 
 def index(request):
@@ -11,11 +12,28 @@ def index(request):
     
     expenses = Expense.objects.all()
     total_expenses = expenses.aggregate(Sum('amount'))
+
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    year_data = Expense.objects.filter(date__gt=last_year)
+    yearly_sum = year_data.aggregate(Sum('amount'))
+
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    month_data = Expense.objects.filter(date__gt=last_year)
+    monthly_sum = month_data.aggregate(Sum('amount'))
+
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    week_data = Expense.objects.filter(date__gt=last_year)
+    weekly_sum = week_data.aggregate(Sum('amount'))
+
+
     expense_form = ExpenseForm()
     context = {
         'form': expense_form, 
         'expenses': expenses,
-        'total_expenses': total_expenses
+        'total_expenses': total_expenses, 
+        'yearly_sum': yearly_sum,
+        'monthly_sum': monthly_sum,
+        'weekly_sum': weekly_sum
         }
     return render(request, 'main_app/index.html', context)
 
